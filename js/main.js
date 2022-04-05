857d7b265e08c7c94ece097e8aead93642dcf68d
Shishkeks <shishalov_yura@mail.ru>
2022-04-05 11:40:09 +0300

    Внес правки

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ const getRandomInteger = function (min, max) {
 
 getRandomInteger();
 
-const MESSAGES_COMMENTS = [
+const MESSAGE_COMMENTS = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -34,7 +34,7 @@ const MESSAGES_COMMENTS = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
-const NAMES_USERS = [
+const USER_NAMES = [
   'Виталий',
   'Иван',
   'Николай',
@@ -43,38 +43,19 @@ const NAMES_USERS = [
   'Виктор'
 ];
 const MAX_ITEM_ID = 25;
-const IMG_FOLDER = 'photos/';
-const IMG_EXTEND = '.jpg';
 const MIN_LIKES = 15;
 const MAX_LIKES = 200;
-const AVATAR_FOLDER = 'img/avatar-';
-const AVATAR_EXTEND = '.svg';
 const getRandomInt = function (min = 0, max = 0) {
   return Math.floor(Math.random() * (max - min) + min);
 };
-
-function shuffle(array) {
-  array.sort(() => Math.random() - 0.5);
-}
-
-let counter = 0;
-const getSuffleIntArray = function (length) {
-  const arr = [];
-  for (let integer = 0; integer < length; integer++) {
-    arr.push(integer);
-  }
-  shuffle(arr);
-  return arr;
-};
-const suffleArray = getSuffleIntArray(1000);
 const createRandomComments = function (maxLength) {
   const result = [];
   for (let i = 0; i < maxLength; i++) {
     result.push({
       id: suffleArray[counter++],
-      avatar: AVATAR_FOLDER + getRandomInt(1, 6) + AVATAR_EXTEND,
-      message: MESSAGES_COMMENTS[getRandomInt(0, MESSAGES_COMMENTS.length - 1)],
-      name: NAMES_USERS[getRandomInt(0, NAMES_USERS.length - 1)],
+      avatar: 'img/avatar-${random}.svg',
+      message: MESSAGE_COMMENTS[getRandomInt(0, MESSAGE_COMMENTS.length - 1)],
+      name: USER_NAMES[getRandomInt(0, USER_NAMES.length - 1)],
     });
   }
   return result;
@@ -84,7 +65,7 @@ const createRandomPhoto = function (maxLength) {
   for (let i = 1; i <= maxLength; i++) {
     result.push({
       id: i,
-      url: IMG_FOLDER + i + IMG_EXTEND,
+      url: 'photos/${random}.jpg',
       description: 'Лучшее фото',
       likes: getRandomInt(MIN_LIKES, MAX_LIKES),
       comments: createRandomComments(getRandomInt(2, 5)),
